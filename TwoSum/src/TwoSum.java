5b831fd451c32dd739801759843ff9a528891c8d
EricHsu <zx327@nyu.edu>
2015-05-14 18:33:22 -0700

    I have created our first project.

---
+++ TwoSum/src/TwoSum.java
@@ -0,0 +1,28 @@
+
+public class TwoSum {
+	public int[] twoSum(int[] numbers, int target) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+		java.util.HashMap<Integer, Integer> map = new java.util.HashMap<Integer, Integer>();
+		int n = numbers.length;
+		int[] result = new int[2];
+		for (int i = 0; i < numbers.length; i++)
+        {
+            if (map.containsKey(target - numbers[i]))
+            {
+                result[0] = map.get(target-numbers[i]) + 1;
+                result[1] = i + 1;
+                break;
+            }
+            else
+            {
+                map.put(numbers[i], i);
+            }
+        }
+		return result;   
+    }
+	
+	public static void main (){
+		
+	}
+}
